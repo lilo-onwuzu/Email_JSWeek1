1ffa0f9c9481ec093c55bd82f5a52b4b3c109ea0
DJ Roychoudhury and Lilo Onwuzu <dj@>
2016-05-23 14:23:39 -0700

    add build gulp task to either minify production js files or run jsBrowserify directly

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,36 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var uglify = require ('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var buildProduction = utilities.env.production;
+
 
 gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/message.js', './js/message-interface.js']})
+  // list front-end files here. back-end js files are "required"/included in the front-end js
+  return browserify({ entries: ['./js/message-interface.js']})
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+  return gulp.src('./build/js/app.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+// running gulp build -- production in the terminal creates an "environment varibale" that sets var buildProduction to true
+// running gulp build won't minifyScripts and just runs the 'justBrowserify' branch
+gulp.task('build', function() {
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify')
+  }
+});
+
+gulp.task('clean', function() {
+  return del(['build']);
 })
